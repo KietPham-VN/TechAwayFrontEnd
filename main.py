d6af849cd52808697477e806c093134ae6475a82
KietPham-VN <phamanhkiet.dev@gmail.com>
2025-03-23 00:07:33 +0700

    final touch

--- main.py
+++ main.py
@@ -77,11 +77,15 @@ with st.sidebar:
         }
     </style>
     <div class="toc">
-        <p>1. Giới thiệu tổng quát V8 Engine</p>
-        <p>2. Fast Property Access</p>
-        <p>3. Garbage Collection</p>
-        <p>4. Pipeline</p>
-        <p>5. Tổng kết</p>
+        <p>1. Lời mở đầu</p>
+        <p>2. Tăng tốc lập trình với IDE hiệu quả</p>
+        <p>3. Top 4 công cụ AI mạnh mẽ hỗ trợ lập trình</p>
+        <p>4. Các Extension hữu ích cho VSCode đối với dân FrontEnd</p>
+        <p>5. React 19 - Sự thay đổi mạnh mẽ</p>
+        <p>6. Thư viện hỗ trợ dự án React trở nên xịn sò hơn</p>
+        <p>7. Cách làm 1 project FrontEnd hiệu quả</p>
+        <p>8. Tổng kết</p>
+                                  
     </div>
     """, unsafe_allow_html=True)
 
@@ -97,55 +101,52 @@ with tab1:
     """)
 
 st.markdown("""
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-0.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-1.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-2.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-3.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-4.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-5.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-6.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-7.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-8.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-9.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-10.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-11.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-12.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-13.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-14.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-15.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-16.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-17.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-18.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-19.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-20.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-21.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-22.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-23.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-24.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-25.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-26.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-27.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-28.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-29.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-30.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-31.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-32.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-33.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-34.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-35.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-36.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-37.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-38.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-39.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-40.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-41.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-42.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-43.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-44.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-45.png)
-![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-46.png)
-            
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-0.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-1.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-2.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-3.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-4.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-5.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-6.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-7.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-8.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-9.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-10.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-11.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-12.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-13.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-14.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-15.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-16.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-17.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-18.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-19.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-20.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-21.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-22.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-23.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-24.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-25.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-26.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-27.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-28.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-29.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-30.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-31.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-32.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-33.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-34.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-35.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-36.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-37.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-38.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-39.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-40.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-41.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-42.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-43.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-44.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-45.jpg)
+![Sprint](https://raw.githubusercontent.com/KietPham-VN/TechAwayFrontEnd/main/assets/images/content-images-46.jpg)
+""", unsafe_allow_html=True)
 
-
-
-""")
